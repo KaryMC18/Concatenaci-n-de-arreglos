8bd516c8e00783aa8c434b3b942a8a80ed262f86
KaryMC18 <kayamacedoca@ittepic.edu.mx>
2024-08-28 23:03:26 -0700

    first

---
+++ Concatenacion.js
@@ -0,0 +1,10 @@
+function concatenarArreglos() {
+    const arreglo1 = ["Hola", "¿Cómo", "estás?"];
+    const arreglo2 = ["Estoy", "bien,", "gracias"];
+
+    const arregloConca = arreglo1.concat(arreglo2);
+
+    console.log("Arreglo concatenado:", arregloConca);
+  }
+  concatenarArreglos();
+  
